59eaa5ca1b6027110cad4809c28ea1254e81fca8
WebRizen <webrizen@gmail.com>
2023-11-11 11:25:11 +0530

    check teams updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <>
-      <header className="text-gray-600 body-font border-b border-b-gray-300 backdrop-blur-2xl">
+      <header className="text-gray-600 body-font backdrop-blur-2xl sticky top-0 z-50">
         <div className="container mx-auto flex p-2 justify-between flex-row items-center">
           <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <Image src={Logo} alt="Aapda." className="h-10 w-auto" />
